328f91779f527a6e4f775ed6a350bb2fc9a7cbb8
mashixaal <safwan7342@gmail.com>
2022-07-28 12:08:08 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("script ok");
+
+let button = document.getElementById("submit-form");
+
+let lastname = document.getElementById("lastname")
+
+let result = document.getElementById("result")
+
+console.log(button,lastname,result);
+
+
+function test(params) {
+    let inner = lastname.value;
+    result.innerHTML = inner;
+}
+
+button.addEventListener("click", test)
